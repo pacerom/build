8445431371e3bc5fb7961aa5e2575d4ef4168be0
Konsta <konsta09@gmail.com>
2013-08-05 23:32:59 +0300

    add cortex-a5 cpu variant
    Change-Id: I369e499d537888eb0f07d900cb50d1424632820e

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -18,6 +18,9 @@ else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7
 else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a5)
+	arch_variant_cflags := -mcpu=cortex-a5
+else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),krait)
 	arch_variant_cflags := -mcpu=cortex-a9
 else
@@ -25,6 +28,7 @@ ifeq ($(strip $(TARGET_CPU_VARIANT)),scorpion)
 	arch_variant_cflags := -mcpu=cortex-a8
 else
 	arch_variant_cflags := -march=armv7-a
+endif
 endif
 endif
 endif
